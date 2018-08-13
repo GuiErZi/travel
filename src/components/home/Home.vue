ad6cbb4faf388025f0fed82d131ffc89e8aa33f0
fjz <fjz0121@163.com>
2018-08-13 12:55:53 +0800

    'header-component'

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>home</div>
+  <div>
+    <home-header></home-header>
+  </div>
 </template>
 
 <script>
+import homeHeader from './components/Header'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    homeHeader
+  }
 }
 </script>
 
---
+++ src/components/home/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+<div class="header">
+  <div class="header-left">
+    <span class="iconfont">&#xe625;</span>
+  </div>
+  <div class="header-input">
+    <span class="iconfont search-icon">&#xe63a;</span>输入城市/景点
+  </div>
+  <div class="header-right">
+    城市<span class="iconfont arrow-icon">&#xe64a;</span>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'homeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+  .header
+    line-height: .86rem
+    display: flex
+    background: $bgColor
+    color: #fff
+    font-size: 13px
+    .header-left
+      width: .64rem
+      text-align: center
+      float: left
+    .header-input
+      flex: 1
+      background: #fff
+      color: #aaa
+      border-radius: .1rem
+      margin-top: .12rem
+      height: .64rem
+      line-height: .64rem
+      margin-left: .2rem
+      padding-left: .2rem
+      .search-icon
+        margin-right: .1rem
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      font-size: 15px
+      .arrow-icon
+        font-size: 12px
+        margin-left: .1rem
+</style>
