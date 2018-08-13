90d6c1d5379a6624715f1bb480063672ac030965
fjz <fjz0121@163.com>
2018-08-13 13:32:58 +0800

    'add-swiper-component'

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 import homeHeader from './components/Header'
+import homeSwiper from './components/Swiper'
 export default {
   name: 'Home',
   components: {
-    homeHeader
+    homeHeader,
+    homeSwiper
   }
 }
 </script>
---
+++ src/components/home/components/Swiper.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="wrapper">
+  <swiper :options="swiperOption" ref="mySwiper">
+    <!-- slides -->
+    <swiper-slide v-for="swiperList of swiperLists" v-bind:key="swiperList.id">
+         <img class="swiper-img" :src="swiperList.imgUrl">
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'homeSwiper',
+  data: function () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperLists: [
+        {
+          id: '0001',
+          imgUrl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/20180716_wap_11043.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/dujia_shuqi_banner_20180710.jpg'
+        },
+        {
+          id: '0003',
+          imgUrl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/anquan_20180716.png'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    width: 100%
+    height: 0
+    overflow: hidden
+    padding-bottom: 23.4%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
